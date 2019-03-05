04f3a87f1706e24ac6d14c62acfc4b2ec0d1a318
NightQnEarth <Denis1.618@yandex.ru>
2019-03-05 23:19:36 +0500

    Read data from input

--- CAFirstTask/Program.cs
+++ CAFirstTask/Program.cs
@@ -1,9 +1,27 @@
-﻿namespace CAFirstTask
-{
-    internal class Program
+﻿using System;
+// ReSharper disable PossibleNullReferenceException
+
+namespace CAFirstTask
+{    
+    class Program
     {
         public static void Main(string[] args)
         {
+            var rowCount = int.Parse(Console.ReadLine().Trim());
+            var columnCount = int.Parse(Console.ReadLine().Trim());
+            var matrix = new int[rowCount,columnCount];
+
+            for (int row = 0; row < rowCount; row++)
+            {
+                var currentRow = Console.ReadLine().Split();
+                for (int column = 0; column < columnCount; column++)
+                    matrix[row, column] = int.Parse(currentRow[column]);
+            }
+ 
+            var startAsArray = Console.ReadLine().Split();
+            var start = (row: int.Parse(startAsArray[0]), column: int.Parse(startAsArray[1]));
+            var finishAsArray = Console.ReadLine().Split();
+            var finish = (row : int.Parse(finishAsArray[0]), column : int.Parse(finishAsArray[1]));
         }
     }
 }
